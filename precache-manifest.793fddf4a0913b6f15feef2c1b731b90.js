c0d593ca39db29887132721de043931628269f92
wkogut93 <w.kogut@ideo.pl>
2019-06-06 10:49:59 +0200

    Updates

---
+++ precache-manifest.793fddf4a0913b6f15feef2c1b731b90.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42b3a31cf31069a585a895beb128ffc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88819d63de1e41d72d58",
+    "url": "/static/css/main.17045723.chunk.css"
+  },
+  {
+    "revision": "f1881dbf2c28a53196d6",
+    "url": "/static/js/2.3aa3a272.chunk.js"
+  },
+  {
+    "revision": "88819d63de1e41d72d58",
+    "url": "/static/js/main.5a19a0a5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
